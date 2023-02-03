d2a0ae9567490ddc81692ef569f8ab49ad541dcc
Hochul Song <hocsong@c5r4s4.42seoul.kr>
2023-02-03 20:02:31 +0900

    큰 따옴표 이후에 delimiter가 나오는 경우에 발생하는 버그를 잡기 위해 코드를 일부분 수정함. 추가적으로 수정이 필요함.

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 15:55:07 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/03 19:41:09 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/03 19:59:41 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 # include <unistd.h>
 
-# define NO_QUOTE 0
-# define SINGLE_QUOTE 1
-# define DOUBLE_QUOTE 2
+# define NO_QUOTE -1
 
-int     has_quote(char const *s, char *c, int start_idx);
+# define SWITCH_OFF 0
+# define SWITCH_ON 1
+
+int     *find_quote(char const *s, char *c, int start_idx);
 int     get_word_len(char const *s, char *c, int start_idx);
 char    **ft_split(char const *s, char *c);
 
--- srcs/ft_split2.c
+++ srcs/ft_split2.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 19:13:43 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/03 19:28:31 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/03 20:00:06 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,54 @@
 int	get_word_len(char const *s, char *c, int start_idx)
 {
 	int	i;
-    int quote_idx;
+    int should_detect_delimiter;
+    int *quote_idx_val;
 
 	i = start_idx;
-    quote_idx = -1;
-    if (has_quote(s, c, start_idx) == NO_QUOTE)
+    quote_idx_val = find_quote(s, c, start_idx);
+    if (quote_idx_val[0] == NO_QUOTE)
     {
         while (s[i] && !is_delimiter(s, c, i))
 		    i++;
+        free(quote_idx_val);
         return (i - start_idx);
     }
     else
     {
-        while(s[i])
+        should_detect_delimiter = SWITCH_OFF;
+        i = quote_idx_val[0] + 1;
+        while (s[i])
         {
-            if (s[i] == '\'' || s[i] == '\"')
-                quote_idx = i;
-            if (quote_idx != -1 && (s[i] == '\'' || s[i] == '\"'))
-                return (i - start_idx);
+            if (is_quote(s[i]))
+                should_detect_delimiter = SWITCH_ON;
             i++;
         }
     }
 }
 
-int  has_quote(char const *s, char *c, int start_idx)
+int  *find_quote(char const *s, char *c, int start_idx)
 {
     int i;
+    int *idx_value;
 
     i = 0;
+    idx_value = malloc(sizeof (int) * 2);
+    if (!idx_value)
+        exit (1); // 적절한 코드로 바꾸자.
     while (s[i])
     {
-        if (s[i] == '\'')
-            return (SINGLE_QUOTE);
-        if (s[i] == '\"')
-            return (DOUBLE_QUOTE);
+        if (s[i] == '\'' || s[i] == '\"')
+        {
+            idx_value[0] = i;
+            if (s[i] == '\'')
+                idx_value[1] = '\'';
+            if (s[i] == '\"')
+                idx_value[1] = '\"';
+            return (idx_value);
+        }
         i++;
     }
-    return (NO_QUOTE);
+    idx_value[0] == NO_QUOTE;
+    return (idx_value);
 }
+
