b42cb4439d74d5f6cc92e6b50d1faf91116b3533
Hyunjun Kim <hyunjuki@c5r4s4.42seoul.kr>
2023-02-04 11:29:52 +0900

    constants.h를 추가하여 상수를 따로 모아놓음. 기존 헤더에서 split변경

--- include/constants.h
+++ include/constants.h
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 15:55:07 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/04 11:29:22 by hyunjuki         ###   ########.fr       */
+/*   Created: 2023/02/04 11:28:13 by hyunjuki          #+#    #+#             */
+/*   Updated: 2023/02/04 11:29:19 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
 
-# include "constants.h"
-# include "prompt.h"
-
-int		init(void);
-int		*find_quote(char const *s, char *c, int start_idx);
-int		get_word_len(char const *s, char *c, int start_idx);
-char	**ft_tokenize(char const *s, char *c);
+enum e_token_type
+{
+	NO_QUOTE = -1,
+	SWITCH_OFF = 0,
+	SWITCH_ON = 1,
+};
 
 #endif
