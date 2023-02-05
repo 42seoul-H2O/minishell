2ccb07975380f95fcccda0ecf4150794fc8aad5b
Hochul Song <hocsong@c5r4s5.42seoul.kr>
2023-02-05 20:51:17 +0900

    convert_dollar_to_env 함수를 테스트할 코드의 일부를 작성함. 아직 미완성

--- tests/srcs/test_convert_dollar.c
+++ tests/srcs/test_convert_dollar.c
@@ -6,12 +6,21 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 18:33:59 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/05 19:16:48 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/05 20:49:43 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "minishell.h"
+#include "parser.h"
+
+static void	convert_dollar_test(char *s);
+static void	init_envp(char **envp);
+
+void	test_convert_dollar(void)
 {
+	printf("---- Convert_dollar Test Cases ----\n");
+	convert_dollar_test("blahblah $");
+	printf("---- End of Convert_dollar ----\n\n");
 	// blahblah $
 	// blahblah $"dff"
 	// blahblah$
@@ -22,4 +31,27 @@ int	main(void)
 	// $.
 	// $,
 	// $'' $이후에 바로 
+}
+
+static void	convert_dollar_test(char *s)
+{
+	t_str	str;
+	char	**envp;
+
+	init_t_str(&str, s);
+	// init_envp(envp);
+	printf("---- Test Case: %s, with an empty envp\n", s);
+	envp[0] = NULL;
+	convert_dollar_to_env(&str, envp);
+	printf("---- Output ----\n");
+}
+
+static void	init_envp(char **envp)
+{
+	envp[0] = "SHELL=/bin/bash";
+	envp[1] = "PWD=Users/hocsong/Documents/minishell/ms1";
+	envp[2] = "LOGNAME=hocsong";
+	envp[3] = "LaunchInstanceID=1525446E-700F-4494-A67E-8DD2AF55B79B";
+	envp[4] = "HOME=/Users/hocsong";
+	envp[5] = NULL;
 }
