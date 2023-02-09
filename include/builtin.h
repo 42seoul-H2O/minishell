0e9403be6deaad4085452c466a7f55140421f932
Hyunjun Kim <hyunjuki@c5r4s1.42seoul.kr>
2023-02-09 18:40:43 +0900

    unset의 인자로 리스트를 받도록 수정

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:29:34 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/09 18:36:53 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/09 18:39:17 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ void	builtin_pwd(void);
 void	builtin_cd(t_list *node);
 void	builtin_export(t_list *node, t_vararr *env);
 void	builtin_export_check_args(char *argument, t_vararr *env);
+void	builtin_unset(t_list *node, t_vararr *env);
 void	builtin_env(char **input, t_vararr *env);
 void	builtin_echo(char **input, t_vararr *env);
-void	builtin_unset(char **input, t_vararr *env);
 
 void	swap_element(char **a, char **b);
 
