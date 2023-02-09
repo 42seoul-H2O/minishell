c2f58542c01c609beca63f094e0b6de24b920a24
Hyunjun Kim <hyunjuki@c5r4s1.42seoul.kr>
2023-02-09 17:53:35 +0900

    커맨드 타입 저장을 위한 상수 선언

--- include/constants.h
+++ include/constants.h
@@ -6,24 +6,24 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 11:28:13 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/04 15:27:26 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/09 17:53:15 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
+enum e_cmd_type
+{
+	BUILTIN,
+	EXECUTABLE,
+};
+
 enum e_token_type
 {
 	NO_QUOTE = -1,
 	SWITCH_OFF = 0,
 	SWITCH_ON = 1,
-	PIPE,
-	REDIRECTION,
-	HEREDOC,
-	COMMAND,
-	BUILTIN,
-	ARGUMENT,
 };
 
 #endif
