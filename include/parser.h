672833b64b609899e087f0af5980785274ded6c6
Hochul Song <hocsong@c5r4s3.42seoul.kr>
2023-02-10 16:38:37 +0900

    헤더 파일에서 필요없는 내용을 삭제함.

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:11:02 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/10 15:57:32 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/10 15:58:44 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ typedef struct s_str{
 	int		word_count;
 }t_str;
 
-// char	**tokenize(char const *s);
 void	init_t_str(t_str *str, char *s);
 char	**my_split(t_str *str);
 void	set_token_indices(t_str *str);
@@ -44,6 +43,7 @@ int		get_word_len(t_str *str, int start_idx);
 int		is_quoted(t_str *str, int idx);
 int		is_special_delimiter(t_str *str, int idx);
 int		is_delimiter(t_str *str, int idx);
+
 int		replace_dollar_with_env(t_str *str, int word_i, t_dollar_sign *dollar, \
 		int *visited);
 void	convert_dollar_to_env(t_str *str, t_vararr *env);
