42a50758ca3f570a61e6510a59e7424348d19796
Hochul Song <hocsong@c5r4s3.42seoul.kr>
2023-02-12 15:08:22 +0900

    필요 이상으로 free를 하면서 생겼던 메모리 버그를 잡음.

--- srcs/remove_quotes.c
+++ srcs/remove_quotes.c
@@ -6,14 +6,14 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 15:57:18 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/12 14:43:59 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/12 15:07:01 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "builtin.h"
 
-static void	del_char(char **s, size_t i);
+static void	del_char(t_str *str, size_t i);
 static void	del_quotes(t_str *str);
 
 void	remove_quotes(t_str *str)
@@ -29,47 +29,46 @@ void	remove_quotes(t_str *str)
 			builtin_exit(12);
 		init_t_str(temp, str -> words[i]);
 		del_quotes(temp);
-		free(str -> words[i]);
 		str -> words[i] = temp -> s;
 		free(temp);
 		i++;
 	}
 }
 
-static void	del_char(char **s, size_t i)
+static void	del_char(t_str *str, size_t i)
 {
 	char	*pre;
 	char	*post;
 	char	*new;
 
-	pre = malloc(sizeof (char) * ft_strlen(*s) + 1);
-	post = malloc(sizeof (char) * ft_strlen(*s) + 1);
+	pre = malloc(sizeof (char) * ft_strlen(str -> s) + 1);
+	post = malloc(sizeof (char) * ft_strlen(str -> s)) + 1;
 	if (!pre || !post)
 		builtin_exit(12);
 	if (i == 0)
 		pre[0] = 0;
 	else
-		ft_strlcpy(pre, *s, i);
-	ft_strlcpy(post, *s + i + 1, ft_strlen(*s));
+		ft_strlcpy(pre, str -> s, i);
+	ft_strlcpy(post, str -> s + i + 1, ft_strlen(str -> s));
 	new = ft_strjoin(pre, post);
 	free(pre);
 	free(post);
-	free(*s);
-	*s = new;
+	free(str -> s);
+	str -> s = new;
 }
 
 static void	del_quotes(t_str *str)
 {
-	size_t	i;
+	int	i;
 
-	i = ft_strlen(str -> s) - 1;
+	i = (int) ft_strlen(str -> s) - 1;
 	if (i < 0)
 		return ;
 	while (0 <= i)
 	{
 		if ((str -> s[i] == '\'' || str -> s[i] == '\"') && \
 			!is_quoted(str, i))
-			del_char(&(str -> s), i);
+			del_char(str, i);
 		i--;
 	}
 }
