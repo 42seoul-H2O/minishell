e629f63af832a28764b9580cde27559931376e7e
Hochul Song <hocsong@c5r4s3.42seoul.kr>
2023-02-12 14:49:54 +0900

    unquoted 따옴표를 삭제할 때, 기존의 방식처럼 앞에부터 삭제했을 때 index가 꼬이는 문제가 발생해서 뒤에서부터 삭제하도록 개선함.

--- srcs/remove_quotes.c
+++ srcs/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 15:57:18 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/10 16:37:51 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/12 14:43:59 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,23 @@
 #include "builtin.h"
 
 static void	del_char(char **s, size_t i);
+static void	del_quotes(t_str *str);
 
 void	remove_quotes(t_str *str)
 {
 	size_t	i;
-	size_t	j;
 	t_str	*temp;
 
 	i = 0;
 	while (str -> words[i])
 	{
-		j = 0;
 		temp = malloc(sizeof (t_str));
+		if (!temp)
+			builtin_exit(12);
 		init_t_str(temp, str -> words[i]);
-		while (temp -> s[j])
-		{
-			if ((temp -> s[j] == '\'' || temp -> s[j] == '\"') \
-				&& is_quoted(temp, j))
-				del_char(str -> words + i, j);
-			j++;
-		}
+		del_quotes(temp);
+		free(str -> words[i]);
+		str -> words[i] = temp -> s;
 		free(temp);
 		i++;
 	}
@@ -59,4 +56,20 @@ static void	del_char(char **s, size_t i)
 	free(post);
 	free(*s);
 	*s = new;
+}
+
+static void	del_quotes(t_str *str)
+{
+	size_t	i;
+
+	i = ft_strlen(str -> s) - 1;
+	if (i < 0)
+		return ;
+	while (0 <= i)
+	{
+		if ((str -> s[i] == '\'' || str -> s[i] == '\"') && \
+			!is_quoted(str, i))
+			del_char(&(str -> s), i);
+		i--;
+	}
 }
