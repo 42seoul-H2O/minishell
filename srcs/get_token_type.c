44d8a2432e45f0134d537908c77993e2bd4d02df
Hochul Song <hocsong@cx2r3s8.42seoul.kr>
2023-02-15 20:08:18 +0900

    동일한 redirection operator가 나오는 경우 병합하여 하나의 토큰을 구성하도록 개선함.

--- srcs/get_token_type.c
+++ srcs/get_token_type.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 14:55:10 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/15 15:48:06 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/15 20:06:31 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,35 @@
 static int	get_optype(char *word);
 
 //TODO: << >> 와 < < > > 를 구분할 수 있어야 하는데, 지금 방법으로는 구분하지 않는다. 그렇기에,
-	// 원래 문자열을 참고해서 
+	// 원래 문자열을 참고해서 리디렉션 연산자 쌍 사이에 다른 delimiter/special_delimiter가 있는지 체크해야한다.
+	// 이러면 연산자가 3 개 같이 나올 때 처리가 될런지 모르겠네. "<< <", "<<<", "<<|<", "<|<", "< <"
+
+// t_cmd_list	*init_cmd_list(t_str *str)
+// {
+	
+// }
+
+int	merge_redir_tokens(char **words, int idx)
+{
+	if (get_token_type(words, idx) == REDIR_IN \
+		&& get_token_type(words, idx + 1) == REDIR_IN)
+		merge_helper(words, idx);
+	if (get_token_type(words, idx) == REDIR_OUT \
+		&& get_token_type(words, idx + 1) == REDIR_IN)
+		merge_helper(words, idx);
+}
+
+static int	merge_helper(char **words, int idx)
+{
+	words[idx] = ft_strjoin(words[idx], words[idx + 1]);
+	idx++;
+	while (words[idx])
+	{
+		words[idx] = words[idx + 1];
+		idx++;
+	}
+	words[idx] = 0;
+}
 
 int	get_token_type(char **words, int idx)
 {
@@ -29,7 +57,8 @@ int	get_token_type(char **words, int idx)
 		return (optype);
 	else if (word_type)
 		return (word_type);
-	return (0);
+	write(2, "Something went wrong with get_token_type\n", 41);
+	builtin_exit(12);
 }
 
 static int	get_optype(char *word)
