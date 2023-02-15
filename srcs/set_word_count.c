cba496274630ed3ce7227349cead25fabc786a13
Hochul Song <hocsong@cx2r3s8.42seoul.kr>
2023-02-15 12:09:07 +0900

    워드 카운팅을 잘못하던 버그를 잡음.

--- srcs/set_word_count.c
+++ srcs/set_word_count.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 11:22:41 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/05 13:28:12 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/15 12:06:52 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	set_word_count(t_str *str)
 	count = 0;
 	while (str -> s[i])
 	{
-		if (!is_delimiter(str, i)
-			&& is_delimiter(str, i + 1))
+		if (is_delimiter(str, i - 1)
+			&& !is_delimiter(str, i))
 			count++;
-		if (is_special_delimiter(str, i))
+		else if (is_special_delimiter(str, i))
 			count++;
 		i++;
 	}
