15a73a5c679ac70d853af4db879f9be50d2740e0
HyunJun KIM <hihj070914@icloud.com>
2023-02-17 21:08:13 +0900

    args를 가변 배열에서 고정 배열로 바꿈에 따른 빌트인 unset 동작의 변경

--- srcs/builtin_unset.c
+++ srcs/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 14:49:21 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/11 14:57:19 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/17 21:07:28 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	builtin_unset(t_cmdlist *node, t_vararr *env)
 	int	i;
 
 	i = 1;
-	while (i < node->args->len)
+	while (i < node->args->word_count)
 	{
-		ft_unsetenv(env, get_element(node->args, i));
+		ft_unsetenv(env, node->args->words[i]);
 		i++;
 	}
 }
