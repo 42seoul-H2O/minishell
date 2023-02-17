153bf07e4ecc47e76b2f5f7dc7df8f11f4b2370d
HyunJun KIM <hihj070914@icloud.com>
2023-02-17 21:08:59 +0900

    args를 가변 배열에서 고정 배열로 바꿈에 따른 빌트인 env 동작의 변경

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:23:42 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/17 11:13:59 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/17 21:08:48 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	builtin_env(t_cmdlist *node, t_vararr *env)
 {
-	if (get_element(node->args, 1) == NULL)
+	if (node->args->word_count == 1)
 		print_all_string(NULL, env->arr, 0, 0);
 	else
 		ft_putstr_fd("h2osh: env: too many argument\n", 2);
