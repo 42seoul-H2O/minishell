73b54ade27e5c6891141d6b7dc0f62ad04dec3e2
HyunJun KIM <hihj070914@icloud.com>
2023-02-17 21:09:44 +0900

    args를 가변 배열에서 고정 배열로 바꿈에 따른 빌트인 cd 동작의 변경

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:12:53 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/17 11:50:34 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/17 21:09:38 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void	builtin_cd(t_cmdlist *node)
 {
-	if (node->args->len == 1)
+	if (node->args->word_count == 1)
 	{
 		ft_putstr_fd("h2osh: cd: argument not found\n", 2);
 		return ;
 	}
-	if (node->args->len != 2)
+	if (node->args->word_count != 2)
 	{
 		ft_putstr_fd("h2osh: cd: too many arguments\n", 2);
 		return ;
 	}
-	if (access(get_element(node->args, 1), F_OK) == 0)
-		if (chdir(get_element(node->args, 1)) == 0)
+	if (access(node->args->words[1], F_OK) == 0)
+		if (chdir(node->args->words[1]) == 0)
 			return ;
-	ft_putstr_fd("h2osh: cd: ", 1);
-	ft_putstr_fd(get_element(node->args, 1), 1);
+	ft_putstr_fd("h2osh: cd: ", 2);
+	ft_putstr_fd(node->args->words[1], 2);
 	ft_putstr_fd(": ", 1);
 	perror(NULL);
 }
