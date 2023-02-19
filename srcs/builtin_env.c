80f7481a1c4de27e4007d393e00b2ef925e9acc3
HyunJun KIM <hihj070914@icloud.com>
2023-02-19 12:13:40 +0900

    env의 에러메시지 수정

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:23:42 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/18 19:42:39 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/19 12:13:14 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	builtin_env(t_cmdlist *node, t_vararr *env)
 	}
 	else
 	{
-		ft_putstr_fd("h2osh: env: too many argument\n", 2);
+		ft_putstr_fd("h2osh: env: too many arguments\n", 2);
 		g_exit_code = 1;
 	}
 }
