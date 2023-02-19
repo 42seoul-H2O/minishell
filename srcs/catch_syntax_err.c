ddb71fb8ece248ccb3ccb856f3e5e544409e5447
Hochul Song <hocsong@cx2r3s7.42seoul.kr>
2023-02-19 15:22:33 +0900

    파이프가 연속으로 존재하거나 리디렉션 연산자가 연속으로 존재할 때만 syntax 에러가 발생하도록 개선함.

--- srcs/catch_syntax_err.c
+++ srcs/catch_syntax_err.c
@@ -6,7 +6,7 @@
 /*   By: hocsong <hocsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 18:59:47 by hocsong           #+#    #+#             */
-/*   Updated: 2023/02/18 19:19:54 by hocsong          ###   ########seoul.kr  */
+/*   Updated: 2023/02/19 15:20:28 by hocsong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static void	catch_pipe_err(t_parsed *parsed);
 static void	catch_no_token_after_op(t_parsed *parsed);
 static void	catch_contiguous_ops(t_parsed *parsed);
+static int	is_redir(char *word);
 
 void	catch_syntax_err(t_parsed *parsed)
 {
@@ -55,12 +56,27 @@ static void	catch_contiguous_ops(t_parsed *parsed)
 	i = 0;
 	while (parsed -> words[i])
 	{
-		if (0 < i && parsed->words[i] && get_optype(parsed -> words[i - 1]) \
-		&& get_optype(parsed -> words[i]))
+		if (0 < i && parsed->words[i] && ((is_redir(parsed -> words[i - 1]) \
+		&& is_redir(parsed -> words[i])) \
+		|| (get_optype(parsed -> words[i - 1]) == PIPE \
+		&& get_optype(parsed -> words[i]) == PIPE)))
 		{
 			parsed -> words_count = -1;
 			return ;
 		}
 		i++;
 	}
+}
+
+static int	is_redir(char *word)
+{
+	int	optype;
+
+	optype = get_optype(word);
+	if (optype == REDIR_IN \
+		|| optype == REDIR_OUT \
+		|| optype == APPEND \
+		|| optype == HEREDOC)
+		return (1);
+	return (0);
 }
