956061cc50ada489b6c6d0314a6b0da0f04e2caa
HyunJun KIM <hihj070914@icloud.com>
2023-02-21 12:13:41 +0900

    S_ISDIR, S_ISREG 매크로 함수를 교체

--- srcs/tools.c
+++ srcs/tools.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 11:17:07 by hyunjuki          #+#    #+#             */
-/*   Updated: 2023/02/21 12:06:54 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2023/02/21 12:12:58 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,25 @@ int	ft_wifsignaled(int stat)
 	return (((stat & 0177) != 0177) && (stat != 0));
 }
 
+static int	ft_isdir(int mode)
+{
+	return ((mode & 0170000) == 0040000);
+}
+
+static int	ft_isreg(int mode)
+{
+	return ((mode & 0170000) == 0100000);
+}
+
 int	ft_access(const char *path)
 {
 	struct stat	s_stat;
 
 	if (stat(path, &s_stat))
 		return (-1);
-	if (S_ISDIR(s_stat.st_mode))
+	if (ft_isdir(s_stat.st_mode))
 		return (-2);
-	if (S_ISREG(s_stat.st_mode))
+	if (ft_isreg(s_stat.st_mode))
 		return (0);
 	return (-1);
 }
